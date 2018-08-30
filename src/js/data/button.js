0667bd16cab50db7d37fb2b8c8e1887ce233f87c
Mihail <miiixsc@gmail.com>
2018-08-30 16:10:10 +0300

    *

---
+++ src/js/data/button.js
@@ -0,0 +1,3 @@
+const button = `Button`;
+
+export default button;
--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,71 @@
-const windowFactory = document.querySelector(`.factory`);
+const buttonTemplate = document.querySelector(`#template`).content;
 
-// окно настроек
-const windowSettings = windowFactory.querySelector(`.settings`);
-const settingsWidth = windowSettings.querySelector(`#settingsWidth`);
-const settingsText = windowSettings.querySelector(`#settingsText`);
-const settingsBgColor = windowSettings.querySelector(`#settingsBgColor`);
+const createButton = (settings, template = buttonTemplate) => {
+	const container = template.cloneNode(true);
+	const newButton = document.createElement(`div`);
+	newButton.appendChild(container);
+	const buttonWrapper = container.querySelector(`div`);
+	const button = newButton.querySelector(`a`);
 
-// окно результата
-const windowResult = windowFactory.querySelector(`.result`);
-const resultPreview = windowResult.querySelector(`.result__preview .container`);
-const previewButton = resultPreview.querySelector(`div`);
-const resultCode = windowResult.querySelector(`.result__code .container`);
+	const settingsDefault = {
+		href: `#`,
+		target: `_blank`,
+		width: `240`,
+		height: `32`,
+		fontFamily: `'Segoe UI', 'Helvetica Neue', sans-serif`,
+		fontSize: `16`,
+		color: `#FFFFFF`,
+		bgColor: `red`
+	};
 
-const renderCode = (what, where) => {
-	const code = what.innerHTML.trim(``).split(`&quot;`).join(`'`);
-	console.log(code); // \n
-	where.innerText = code;
+	button.href = settings.href || settingsDefault.href;
+	// button.setAttribute(`href`, settings.href || settingsDefault.href);
+	// button.setAttribute(`target`, settings.target || settingsDefault.target);
+	// button.style.width = settings.width || settingsDefault.width;
+	// button.style.height = settings.height || settingsDefault.height;
+
+	document.querySelector(`.result__preview .container`).appendChild(newButton);
 };
 
-// Ширина кнопки
-settingsWidth.addEventListener(`change`, () => {
-	previewButton.querySelector(`a`).style.width = `${settingsWidth.value}px`;
-	renderCode(resultPreview, resultCode);
-});
+createButton();
 
-// текст кнопки
-settingsText.addEventListener(`change`, () => {
-	previewButton.querySelector(`a`).innerText = settingsText.value;
-	renderCode(resultPreview, resultCode);
-});
+// const windowFactory = document.querySelector(`.factory`);
 
-// фон кнопки
-settingsBgColor.addEventListener(`change`, () => {
-	previewButton.querySelector(`a`).style.backgroundColor = settingsBgColor.value;
-	renderCode(resultPreview, resultCode);
-});
+// // окно настроек
+// const windowSettings = windowFactory.querySelector(`.settings`);
+// const settingsWidth = windowSettings.querySelector(`#settingsWidth`);
+// const settingsText = windowSettings.querySelector(`#settingsText`);
+// const settingsBgColor = windowSettings.querySelector(`#settingsBgColor`);
 
-renderCode(resultPreview, resultCode);
+// // окно результата
+// const windowResult = windowFactory.querySelector(`.result`);
+// const resultPreview = windowResult.querySelector(`.result__preview .container`);
+// const previewButton = resultPreview.querySelector(`div`);
+// const resultCode = windowResult.querySelector(`.result__code .container`);
+
+// const renderCode = (what, where) => {
+// 	const code = what.innerHTML.trim(``).split(`&quot;`).join(`'`);
+// 	console.log(code); // \n
+// 	where.innerText = code;
+// };
+
+// // Ширина кнопки
+// settingsWidth.addEventListener(`change`, () => {
+// 	previewButton.querySelector(`a`).style.width = `${settingsWidth.value}px`;
+// 	renderCode(resultPreview, resultCode);
+// });
+
+// // текст кнопки
+// settingsText.addEventListener(`change`, () => {
+// 	previewButton.querySelector(`a`).innerText = settingsText.value;
+// 	renderCode(resultPreview, resultCode);
+// });
+
+// // фон кнопки
+// settingsBgColor.addEventListener(`change`, () => {
+// 	previewButton.querySelector(`a`).style.backgroundColor = settingsBgColor.value;
+// 	renderCode(resultPreview, resultCode);
+// });
+
+// renderCode(resultPreview, resultCode);
 
