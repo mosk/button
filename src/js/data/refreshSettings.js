e34d6f53bf34d5315b95dd8681c6db2e810a442f
Mihail <miiixsc@gmail.com>
2018-10-16 16:05:04 +0300

    *

--- src/js/data/refreshSettings.js
+++ src/js/data/refreshSettings.js
@@ -2,16 +2,13 @@ import changeButton from './changeButton';
 
 const refreshSettings = (event, settings) => {
 	const propertyPrefix = `settings`;
-	const propertyName = event.target.id.replace(propertyPrefix, ``);
-	const firstLetter = propertyName[0];
-	const property = propertyName.split(firstLetter).join(firstLetter.toLowerCase());
+	let propertyName = event.target.id.replace(propertyPrefix, ``);
+	let firstLetter = propertyName[0].toLowerCase();
+	let propertyWithoutFirstLetter = propertyName.slice(1, propertyName.length);
+
+	let property = firstLetter + propertyWithoutFirstLetter;
 
 	settings[property] = event.target.value;
-
-	// беспредел
-	if (property === `fontfamily`) {
-		settings.fontFamily = event.target.value;
-	}
 
 	console.log(`${propertyName}, ${property}`);
 
